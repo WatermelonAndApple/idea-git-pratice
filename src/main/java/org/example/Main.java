b9589a07e23abadc26aa31ac8a506dcd7ba80475
watermelonandapple <3334059895@qq.com>
2022-10-27 21:12:44 +0800

    main代码冲突

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello IDEA Git提交第二次!");
         System.out.println("hot-fix first commit");
+        System.out.println("main  ");
     }
 }
