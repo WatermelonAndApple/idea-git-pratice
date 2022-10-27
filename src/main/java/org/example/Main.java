7911e3c3037aabc6a0f81b894b4df0b94e536c8e
watermelonandapple <3334059895@qq.com>
2022-10-27 21:04:56 +0800

    IDEA使用hot-fix的第1次测试提交

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello IDEA Git提交第二次!");
+        System.out.println("hot-fix first commit");
     }
 }
